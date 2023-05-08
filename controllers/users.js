07aee9e42af8b2ef0146b0c3c7334091c3b84ad2
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-05-08 17:03:24 +0300

    fix: исправить createUser

--- controllers/users.js
+++ controllers/users.js
@@ -15,39 +15,42 @@ module.exports.login = (req, res, next) => {
       res.cookie("jwt", token, {
         maxAge: 3600000,
         httpOnly: true,
-      }).end();
+      })
+        .send({ email })
+        .end();
     })
     .catch(next);
 };
 
 module.exports.createUser = (req, res, next) => {
   const {
-    name, about, avatar, email, password,
+    name, about, avatar, email,
   } = req.body;
-  bcrypt.hash(password, 10)
+
+  bcrypt.hash(req.body.password, 10)
     .then((hash) => {
       User.create({
         name, about, avatar, email, password: hash,
-      });
-    })
-    .then((user) => res.status(201).send({ data: user }))
-    .catch((err) => {
-      if (err.code === 11000) {
-        next(new ConflictError("Пользователь с таким email уже существует."));
-      } else if (err.name === "ValidationError") {
-        next(new BadRequestError("Некорректные данные при создании пользователя."));
-      } else {
-        next(err);
-      }
+      }).then((user) => res.status(201).send({ data: user }))
+        .catch((err) => {
+          if (err.code === 11000) {
+            next(new ConflictError("Пользователь с таким email уже существует."));
+          } else if (err.name === "ValidationError") {
+            next(new BadRequestError("Некорректные данные при создании пользователя."));
+          } else {
+            next(err);
+          }
+        });
     });
 };
 
 module.exports.getUsers = (req, res, next) => {
-  User.find({}).then((users) => {
-    res
-      .send({ data: users })
-      .catch(next);
-  });
+  User.find({})
+    .then((users) => {
+      res
+        .send({ data: users });
+    })
+    .catch(next);
 };
 
 module.exports.getUser = (req, res, next) => {
